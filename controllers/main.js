35f53f0087f1d5293fa738d51d69103b4312f34d
dvcs_TT <t.a.tuan9981@gmail.com>
2024-03-14 09:12:06 +0700

    2nd version

--- controllers/main.js
+++ controllers/main.js
@@ -1,14 +1,19 @@
-// import * as jsonData from "../data/Data.json";
-// let objData = JSON.parse(jsonData);
-// console.log('objData: ', objData);
-
-import objData from "../data/Data.js";
-console.log("objData: ", objData);
+// import & parse json data to object data
+import objData from "../data/Data.json" assert { type: "json" };
 
 let { navPills, tabPanes } = objData;
+console.log("tabPanes: ", tabPanes);
+
+let typeArray = [];
+tabPanes.forEach((tabValue) => {
+  if (!typeArray.includes(tabValue.type)) {
+    typeArray.push(tabValue.type);
+  }
+});
+console.log("typeArray: ", typeArray);
 
 window.tryOut = (type, imgSrc_png) => {
-  document.querySelector(`#${type}`).style.background = `url("${imgSrc_png}")`;
+  document.querySelector(`#${type}`).style.backgroundImage = `url("${imgSrc_png}")`;
 };
 
 const renderNavPills = (navPillArray) => {
@@ -44,41 +49,51 @@ const renderTabContent = (navPillArray, tabPaneArray) => {
   renderNavPills(navPillArray);
   document.querySelector("#pills-tabContent").innerHTML = tabPaneArray.reduce(
     (acc, value, index) => {
-      return (
-        acc +
-        `
-      <!-- Tab pane | ${value.type} -->
-    <div
-      class="tab-pane fade ${!index ? "show active" : ""}"
-      id="pills-${value.type}"
-      role="tabpanel"
-      aria-labelledby="pills-${value.type}-tab"
-      tabindex="0"
-    >
-      <!-- tab content | ${value.type} -->
-      <div class="row" id="tabContent_${value.type}">
-      ${tabPaneArray.filter((value) => value.type).reduce((acc, value) => {
-        return (
-          acc +
-          `<div class="col-3">
-            <div class="card text-center">
-              <img
-                class="card-img-top hovercard"
-                src="${value.imgSrc_jpg}"
-                alt="${value.name}"
-              />
-              <div class="card-body p-0">
-                <h4 class="card-title fw-bold">${value.name}</h4>
-                <button class="btn w-100" onclick="tryOut(${value.type}, ${value.imgSrc_png})">Thử đồ</button>
+      // Loop through the type array
+      for (let i = 0; i < typeArray.length; i++) {
+        const currentType = typeArray[i];
+
+        // Check if current index matches the loop counter (alternate types)
+        if (index % typeArray.length === i) {
+          return (
+            acc +
+            `
+              <div
+                class="tab-pane fade ${!index ? "show active" : ""}"
+                id="pills-${currentType}"
+                role="tabpanel"
+                aria-labelledby="pills-${currentType}-tab"
+                tabindex="0"
+              >
+                <div class="row" id="tabContent_${currentType}">
+                  ${tabPaneArray
+                    .filter((value) => value.type === currentType)
+                    .reduce((acc, value) => {
+                      return (
+                        acc +
+                        `<div class="col-3">
+                          <div class="card text-center">
+                            <img
+                              class="card-img-top hovercard"
+                              src="${value.imgSrc_jpg}"
+                              alt="${value.name}"
+                            />
+                            <div class="card-body p-0">
+                              <h4 class="card-title fw-bold">${value.name}</h4>
+                              <button class="btn w-100" onclick="tryOut('${value.type}', '${value.imgSrc_png}')">Thử đồ</button>
+                            </div>
+                          </div>
+                        </div>`
+                      );
+                    }, "")}
+                </div>
               </div>
-            </div>
-          </div>`
-        );
-      }, "")}
-      </div>
-    </div>
-      `
-      );
+            `
+          );
+        }
+      }
+      // No match found, skip to the next iteration
+      return acc;
     },
     ""
   );
